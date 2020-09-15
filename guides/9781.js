999ce1c362cd6705a9e9881ce088b298cd4ca7ca
Loliconera <69399372+Loliconera@users.noreply.github.com>
2020-09-15 14:38:04 -0500

    Arreglos en la Traducción

--- guides/9781.js
+++ guides/9781.js
@@ -90,7 +90,7 @@ module.exports = {
 		{ "type": "spawn_func", "func": "marker", "args": [false, 130, 100, 0, 2000, true, null] }
 	],
 	"s-781-1000-2304-0": [
-		{ "type": "text", "sub_type": "message", "message": "Flying", "message_RU": "Взлет", "message_ES": "Volador" },
+		{ "type": "text", "sub_type": "message", "message": "Flying", "message_RU": "Взлет", "message_ES": "Volar" },
 		{ "type": "spawn_func", "func": "circle", "args": [false, 553, 0, 0, 10, 300, 0, 6000] }
 	],
 	"s-781-1000-2303-0": [{ "type": "text", "sub_type": "message", "message": "Spin", "message_RU": "Крутилка", "message_ES": "Girar" }],
@@ -108,7 +108,7 @@ module.exports = {
 		{ "type": "spawn_func", "func": "marker", "args": [false, 130, 100, 0, 2000, true, null] }
 	],
 	"s-781-1000-1304-0": [
-		{ "type": "text", "sub_type": "message", "message": "Flying", "message_RU": "Взлет", "message_ES": "Volador" },
+		{ "type": "text", "sub_type": "message", "message": "Flying", "message_RU": "Взлет", "message_ES": "Volar" },
 		{ "type": "spawn_func", "func": "circle", "args": [false, 553, 0, 0, 10, 300, 0, 6000] }
 	],
 	"s-781-1000-1303-0": [{ "type": "text", "sub_type": "message", "message": "Spin", "message_RU": "Крутилка", "message_ES": "Girar" }],
@@ -124,8 +124,8 @@ module.exports = {
 	//"s-781-2000-1503-0": [{ "type": "text", "sub_type": "message", "message": "坦快跑远", "message_RU": "坦快跑远" }], // наткнул
 	"s-781-2000-1106-0": [{ "type": "text", "sub_type": "message", "message": "Back", "message_RU": "Задний", "message_ES": "Ataque hacia atrás" }],
 	"s-781-2000-1108-0": [{ "type": "text", "sub_type": "message", "message": "Front", "message_RU": "Передний", "message_ES": "Ataque hacia adelante" }],
-	"s-781-2000-1111-0": [{ "type": "text", "sub_type": "message", "message": "360 attack", "message_RU": "Круговая", "message_ES": "Girar" }],
-	"s-781-2000-1302-0": [{ "type": "text", "sub_type": "message", "message": "Bait", "message_RU": "Байт", "message_ES": "Ataque al jugador" }],
+	"s-781-2000-1111-0": [{ "type": "text", "sub_type": "message", "message": "360 attack", "message_RU": "Круговая", "message_ES": "Ataque 360" }],
+	"s-781-2000-1302-0": [{ "type": "text", "sub_type": "message", "message": "Bait", "message_RU": "Байт", "message_ES": "Ataque hacia al jugador" }],
 	//"s-781-2000-1121-0": [{ "type": "text", "sub_type": "message", "message": "Summon Mobs", "message_RU": "Призыв мобов" }],
 	"s-781-2000-1501-0": [
 		{ "type": "text", "sub_type": "message", "message": "Identification", "message_RU": "Идентификация", "message_ES": "Identificación" },
@@ -146,7 +146,7 @@ module.exports = {
 	//"s-781-2000-2503-0": [{ "type": "text", "sub_type": "message", "message": "坦快跑远", "message_RU": "坦快跑远" }], // дурион выбрал цель
 	"s-781-2000-2106-0": [{ "type": "text", "sub_type": "message", "message": "Back", "message_RU": "Задний", "message_ES": "Ataque hacia atrás" }],
 	"s-781-2000-2108-0": [{ "type": "text", "sub_type": "message", "message": "Front", "message_RU": "Передний", "message_ES": "Ataque hacia adelante" }],
-	"s-781-2000-2111-0": [{ "type": "text", "sub_type": "message", "message": "360 attack", "message_RU": "Круговая", "message_ES": "Girar" }],
+	"s-781-2000-2111-0": [{ "type": "text", "sub_type": "message", "message": "360 attack", "message_RU": "Круговая", "message_ES": "Ataque 360" }],
 	//"s-781-2000-2121-0": [{ "type": "text", "sub_type": "message", "message": "Summon Mobs", "message_RU": "Призыв мобов" }],
 	"s-781-2000-2501-0": [
 		{ "type": "text", "sub_type": "message", "message": "Identification", "message_RU": "Идентификация", "message_ES": "Identificación" },
@@ -185,10 +185,10 @@ module.exports = {
 	//"s-781-3000-2116-0": [{ "type": "text", "sub_type": "message", "message": "Circle", "message_RU": "Круг" }],
 	"s-781-3000-1136-0": [{ "type": "text", "sub_type": "message", "message": "Claw", "message_RU": "Когти", "message_ES": "Garras" }],
 	"s-781-3000-1701-0": [{ "type": "text", "sub_type": "message", "message": "Back + Front", "message_RU": "Назад + Вперед", "message_ES": "Ataque hacia atrás + Adelante" }],
-	"s-781-3000-1113-0": [{ "type": "text", "sub_type": "message", "message": "Bait", "message_RU": "Байт", "message_ES": "Ataque al jugador" }],
+	"s-781-3000-1113-0": [{ "type": "text", "sub_type": "message", "message": "Bait", "message_RU": "Байт", "message_ES": "Ataque hacia al jugador" }],
 	"s-781-3000-1151-0": [{ "type": "text", "sub_type": "message", "message": "Attention Stun", "message_RU": "Стан", "message_ES": "Stun" }],
 	"s-781-3000-2151-0": [{ "type": "text", "sub_type": "message", "message": "Attention Stun", "message_RU": "Стан", "message_ES": "Stun" }],
-	"s-781-3000-2113-0": [{ "type": "text", "sub_type": "message", "message": "Bait", "message_RU": "Байт", "message_ES": "Ataque al jugador" }],
+	"s-781-3000-2113-0": [{ "type": "text", "sub_type": "message", "message": "Bait", "message_RU": "Байт", "message_ES": "Ataque hacia al jugador" }],
 	"s-781-3000-1152-0": [{ "type": "text", "sub_type": "message", "message": "Stun + Back", "message_RU": "Стан + Откид назад", "message_ES": "Stun + Ataque hacia atrás" }],
 	"s-781-3000-2152-0": [{ "type": "text", "sub_type": "message", "message": "Stun + Back", "message_RU": "Стан + Откид назад", "message_ES": "Stun + Ataque hacia atrás" }],
 	"s-781-3000-2138-0": [{ "type": "spawn_func", "func": "circle", "args": [false, 553, 0, 0, 10, 250, 0, 6000] }],
