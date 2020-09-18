f7e326258df6ca44e96f971e5e8ce61d9aefbd21
Loliconera <69399372+Loliconera@users.noreply.github.com>
2020-09-18 11:01:48 -0500

    Actualización

--- guides/9053.js
+++ guides/9053.js
@@ -5,15 +5,15 @@
 let player, entity, library, effect;
 
 module.exports = {
-	load(dispatch){
+	load(dispatch) {
 		({ player, entity, library, effect } = dispatch.require.library);
 	},
 	// Kezzel (1st boss)
-	"s-453-999-103-0": [{ "type": "text", "sub_type": "message", "message": "Smash (Left)", "message_RU": "Удар (лево)", "message_ES": "Golpear (Izquierda)" }],
-	"s-453-999-104-0": [{ "type": "text", "sub_type": "message", "message": "Smash (Right)", "message_RU": "Удар (право)", "message_ES": "Golpear (Derecha)" }],
+	"s-453-999-103-0": [{ "type": "text", "sub_type": "message", "message": "Smash (Left)", "message_RU": "Удар (лево)", "message_ES": "Golpe (Izquierdo)" }],
+	"s-453-999-104-0": [{ "type": "text", "sub_type": "message", "message": "Smash (Right)", "message_RU": "Удар (право)", "message_ES": "Golpe (Derecho)" }],
 	"s-453-999-105-0": [
 		{ "type": "text", "sub_type": "message", "message": "Rock Smash", "message_RU": "Удар (танк)", "message_ES": "Golpe de Rocas", "class_position": "tank" },
-		{ "type": "spawn_func", "func": "circle", "args": [false, 553, 0, 210, 10, 185, 0, 3500] }
+		{ "type": "spawn_func", "func": "circle", "args": [true, 553, 0, 210, 14, 190, 0, 3000] }
 	],
 	"s-453-999-106-0": [
 		{ "type": "text", "sub_type": "message", "message": "Blast", "message_RU": "Выстрел", "message_ES": "Disparar" },
@@ -24,12 +24,12 @@ module.exports = {
 	"s-453-999-119-0": [
 		{ "type": "text", "sub_type": "message", "message": "Kaia's Shield", "message_RU": "Kaia's Shield", "message_ES": "Kaia's Shield", "class_position": "priest" },
 		{ "type": "text", "sub_type": "message", "message": "Thrall of Protection", "message_RU": "Thrall of Protection", "message_ES": "Thrall of Protection", "class_position": "mystic" },
-		{ "type": "spawn_func", "func": "circle", "args": [false, 553, 0, 75, 10, 450, 0, 5700] } // Unsure on size and position, the in-game telegraphs for this boss are buggy
+		{ "type": "spawn_func", "func": "circle", "args": [true, 553, 0, 0, 10, 500, 0, 6000] }
 	],
 	"s-453-999-120-0": [
 		{ "type": "text", "sub_type": "message", "message": "AoE Waves", "message_RU": "AoE волны", "message_ES": "AoE Olas" },
-		{ "type": "spawn_func", "func": "circle", "args": [false, 445, 0, 0, 10, 200, 0, 5700] }, // Inner circle
-		{ "type": "spawn_func", "func": "circle", "args": [false, 445, 0, 0, 10, 390, 0, 6700] }, // Middle circle
-		{ "type": "spawn_func", "func": "circle", "args": [false, 445, 0, 0, 10, 590, 0, 7700] }  // Outer circle
+		{ "type": "spawn_func", "func": "circle", "args": [false, 445, 0, 0, 14, 200, 0, 7000] },
+		{ "type": "spawn_func", "func": "circle", "args": [false, 445, 0, 0, 10, 390, 0, 7000] },
+		{ "type": "spawn_func", "func": "circle", "args": [false, 445, 0, 0, 8, 590, 0, 7000] }
 	]
 };
