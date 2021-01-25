fadee1c9ad02729589b87f6f36dac57d63e4a142
Loliconera <69399372+Loliconera@users.noreply.github.com>
2021-01-25 14:01:24 -0500

    Add files via upload

--- guides/3204.js
+++ guides/3204.js
@@ -1,16 +1,34 @@
-// Catalepticon (Hard)
+// Catalepticon (Difícil)
 //
 // hecho por HSDN
 
 module.exports = (dispatch, handlers, guide, lang) => {
 
+	let combo_count = 0;
+
 	return {
+		"ns-3204-1000": [{ type: "func", func: () => combo_count = 0 }],
 		"nd-3204-1000": [
 			{ type: "stop_timers" },
 			{ type: "despawn_all" }
 		],
 
-		"qb-3204-1000-32042000": [{ type: "text", sub_type: "message", message: "Arrows", message_ES: "Flechas" }],
+		"qb-3204-1000-32041000": [
+			{ type: "text", sub_type: "message", message: "Arrows", message_ES: "Flechas" },
+			{ type: "func", func: () => combo_count = 0 }
+		],
+		"qb-3204-1000-32042000": [{ type: "text", sub_type: "message", message: "Arrows + Line", message_ES: "Flechas + Línea" }],
+		"qb-3204-1000-32042006": [
+			{ type: "text", sub_type: "message", message: "Cage", message_ES: "Jaula" },
+			{ type: "text", sub_type: "alert", message: "Cage soon...", message_ES: "Jaula pronto...", delay: 100000 }
+		],
+		"qb-3204-1000-32042009": [
+			{ type: "text", sub_type: "message", message: "Skeletons", message_ES: "Esqueletos" },
+			{ type: "text", sub_type: "alert", message: "Skeletons soon...", message_ES: "Esqueletos pronto...", delay: 220000 }
+		],
+
+		"h-3204-1000-90": [{ type: "text", sub_type: "message", message: "90%", message_ES: "90%" }],
+		"h-3204-1000-70": [{ type: "text", sub_type: "message", message: "70%", message_ES: "70%" }],
 		"h-3204-1000-35": [{ type: "text", sub_type: "message", message: "35%", message_ES: "35%" }],
 
 		"s-3204-1000-104-0": [
@@ -22,13 +40,17 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "spawn", func: "vector", args: [553, 90, 50, 0, 1000, 0, 2000] },
 			{ type: "spawn", func: "vector", args: [553, 270, 50, 0, 1000, 0, 2000] },
 			{ type: "spawn", func: "vector", args: [553, 90, 50, 180, 1000, 0, 2000] },
-			{ type: "spawn", func: "vector", args: [553, 270, 50, 180, 1000, 0, 2000] }
+			{ type: "spawn", func: "vector", args: [553, 270, 50, 180, 1000, 0, 2000] },
+			{ type: "func", func: () => combo_count++ },
+			{ type: "spawn", func: "marker", args: [false, 0, 150, 0, 3000, true, null], check_func: () => combo_count == 2, delay: 1000 }
 		],
 		"s-3204-1000-110-0": [
-			{ type: "text", sub_type: "message", message: "Target + Wave", message_ES: "Ataque hacia -> (al jugador) + Olas" },
+			{ type: "text", sub_type: "message", message: "Target + Wave", message_ES: "Ataque -> (al objetivo) + Olas" },
 			{ type: "spawn", func: "circle", args: [true, 553, 180, -500, 10, 350, 0, 1500] },
-			{ type: "spawn", func: "vector", args: [553, 180, -20, 120, 500, 0, 2000], delay: 1500 },
-			{ type: "spawn", func: "vector", args: [553, 180, -20, 240, 500, 0, 2000], delay: 1500 }
+			{ type: "spawn", func: "vector", args: [553, 180, -50, 120, 500, 0, 2000], delay: 1500 },
+			{ type: "spawn", func: "vector", args: [553, 180, -50, 240, 500, 0, 2000], delay: 1500 },
+			{ type: "func", func: () => combo_count++ },
+			{ type: "spawn", func: "marker", args: [false, 0, 150, 0, 3000, true, null], check_func: () => combo_count > 1, delay: 2500 }
 		],
 		"s-3204-1000-112-0": [{ type: "text", sub_type: "message", message: "Wave Forward", message_ES: "Ola Delantera" },
 			{ type: "spawn", func: "vector", args: [553, 0, 0, 70, 500, 0, 2000] },
@@ -64,7 +86,7 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "spawn", func: "marker", args: [false, 340, 150, 0, 2000, true, null], delay: 1500 }
 		],
 		"s-3204-1000-156-0": [{ type: "text", sub_type: "message", message: "Get Skulls", message_ES: "Cráneos" }],
-		"s-3204-1000-157-0": [{ type: "text", sub_type: "message", message: "Gather!", message_ES: "¡Reunirse!", delay: 5000 }],
+		"s-3204-1000-157-0": [{ type: "text", sub_type: "message", message: "Gather!", message_ES: "¡Reunirse!", delay: 2000 }],
 		"s-3204-1000-158-0": [{ type: "text", sub_type: "message", message: "Gather!", message_ES: "¡Reunirse!", delay: 5000 }],
 		"s-3204-1000-159-0": [{ type: "text", sub_type: "message", message: "AOE", message_ES: "АОЕ" }]
 	};
