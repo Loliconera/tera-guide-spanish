94ceba9d20217c4b94733cb4014bd2f88583b89e
Loliconera <69399372+Loliconera@users.noreply.github.com>
2021-02-03 17:05:42 -0500

    Add files via upload

--- guides/3104.js
+++ guides/3104.js
@@ -29,7 +29,7 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "spawn", func: "circle", args: [true, 553, 180, -50, 10, 380, 0, 2000] }
 		],
 		"s-3104-1000-107-0": [
-			{ type: "text", sub_type: "message", message: "Line Forward + Side Lines", message_ES: "Línea en medio + Líneas Laterales" },
+			{ type: "text", sub_type: "message", message: "Line Forward + Side Lines", message_ES: "Línea en medio + Líneas Laterales + Pizza" },
 			{ type: "spawn", func: "vector", args: [553, 90, 50, 0, 1000, 0, 2000] },
 			{ type: "spawn", func: "vector", args: [553, 270, 50, 0, 1000, 0, 2000] },
 			{ type: "spawn", func: "vector", args: [553, 90, 50, 180, 1000, 0, 2000] },
@@ -38,14 +38,14 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "spawn", func: "marker", args: [false, 0, 150, 0, 3000, true, null], check_func: () => combo_count == 2, delay: 1000 }
 		],
 		"s-3104-1000-110-0": [
-			{ type: "text", sub_type: "message", message: "Target + Wave", message_ES: "Ataque -> (al objetivo) + Olas" },
+			{ type: "text", sub_type: "message", message: "Target + Wave", message_ES: "Ataque -> al objetivo (Stun) + Ataque hacia atrás" },
 			{ type: "spawn", func: "circle", args: [true, 553, 180, -500, 10, 350, 0, 1500] },
 			{ type: "spawn", func: "vector", args: [553, 180, -50, 120, 500, 0, 2000], delay: 1500 },
 			{ type: "spawn", func: "vector", args: [553, 180, -50, 240, 500, 0, 2000], delay: 1500 },
 			{ type: "func", func: () => combo_count++ },
 			{ type: "spawn", func: "marker", args: [false, 0, 150, 0, 3000, true, null], check_func: () => combo_count == 2, delay: 2500 }
 		],
-		"s-3104-1000-112-0": [{ type: "text", sub_type: "message", message: "Wave Forward", message_ES: "Empuje Delantero" },
+		"s-3104-1000-112-0": [{ type: "text", sub_type: "message", message: "Wave Forward", message_ES: "Empuje Frontal" },
 			{ type: "spawn", func: "vector", args: [553, 0, 0, 70, 500, 0, 2000] },
 			{ type: "spawn", func: "vector", args: [553, 0, 0, 290, 500, 0, 2000] }
 		],
@@ -63,7 +63,7 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "text", sub_type: "message", message: "Stun (AOE)", message_ES: "Stun (АОЕ)", delay: 1500 },
 			{ type: "spawn", func: "circle", args: [true, 553, 180, -100, 10, 700, 0, 2500], delay: 1500 }
 		],
-		"s-3104-1000-123-0": [{ type: "text", sub_type: "message", message: "Spin", message_ES: "Girar" }],
+		"s-3104-1000-123-0": [{ type: "text", sub_type: "message", message: "Spin", message_ES: "Ataque Giratorio" }],
 		"s-3104-1000-125-0": [{ type: "text", sub_type: "message", message: "Stun (Tank)", message_ES: "Stun (Tank)" }],
 		"s-3104-1000-127-0": [
 			{ type: "text", sub_type: "message", message: "Pizza", message_ES: "Pizza" },
