8ddaa2c2c6803401d6ea7dbf2bbbc7d2ecdb73cd
Loliconera <69399372+Loliconera@users.noreply.github.com>
2021-04-02 16:24:33 -0500

    Actualizar guía Manglemire

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports.NetworkMod = function (mod) {
 		mod.require["tera-guide-core"].load(mod, {
 			colors: { gui: {}, general: {} }, // ajustes de color
 			command: ["guide"], // comando del módulo
-			chat_name: "Guía", // establecer el nombre del autor del chat para los avisos
+			chat_name: "Guia", // establecer el nombre del autor del chat para los avisos
 		});
 	} catch (e) {
 		mod.error("¡Advertencia!\n¡El módulo dependiente \"tera-guide-core\" necesario para el correcto funcionamiento del módulo TERA-Guía no está instalado!\nPor favor descargue e instale: https://github.com/hsdn/tera-guide-core\n");
