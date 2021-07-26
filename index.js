889321a7beee009bf83119f369858a4ad47f6ce6
Loliconera <69399372+Loliconera@users.noreply.github.com>
2021-07-26 02:56:59 -0500

    GF v107.2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports.NetworkMod = function (mod) {
 		mod.require["tera-guide-core"].load(mod, {
 			colors: { gui: {}, general: {} }, // ajustes de color
 			command: ["guide"], // comando del módulo
-			chat_name: "Guia", // establecer el nombre del autor del chat para los avisos
+			chat_name: "TERA-Guia", // establecer el nombre del autor del chat para los avisos
 		});
 	} catch (e) {
 		mod.error("¡Advertencia!\n¡El modulo dependiente \"tera-guide-core\" necesario para el correcto funcionamiento del modulo TERA-Guia no está instalado!\nPor favor descargue e instale: https://github.com/hsdn/tera-guide-core\n");
--- lang/dungeons.js
+++ lang/dungeons.js
@@ -1,114 +1,67 @@
 "use strict";
 
-// Nombres de mazmorras
+// Available names for additional languages
 module.exports = {
-
-    	9781: {
-			"es": "Velik's Sanctuary"
-		},
-		9739: {
-			"es": "Red Refuge"
-		},
-		9735: {
-			"es": "RK-9 Kennel"
-		},
-		3034: {
-			"es": "Rampaging RK-9 Kennel"
-		},
-		9920: {
-			"es": "Antaroth's Abyss (Difícil)"
-		},
-		 9982: {
-			"es": "Grotto of Lost Souls (Difícil)"
-		},
-		9044: {
-			"es": "Bahaar's Sanctum"
-		},
-		3201: {
-			"es": "Gossamer Vault (Difícil)"
-		},
-		3023: {
-			"es": "Akalath Quarantine"
-		},
-		3020: {
-			"es": "Sea of Honor"
-		},
-		3026: {
-			"es": "Corrupted Skynest"
-		},
-		3126: {
-			"es": "Corrupted Skynest (Difícil)"
-		},
-		3102: {
-			"es": "Draakon Arena"
-		},
-		3202: {
-			"es": "Draakon Arena (Difícil)"
-		},
-		3027: {
-			"es": "Forbidden Arena [Hagufna]"
-		},
-		3103: {
-			"es": "Forbidden Arena [Undying Warlord]"
-		},
-		3203: {
-			"es": "Forbidden Arena [Nightmare Undying Warlord]"
-		},
-		9053: {
-			"es": "Kezzel's Gorge (5-Person)"
-		},
-		7011: {
-			"es": "Shadow of the Gutrends (Guardian)"
-		},
-		7015: {
-			"es": "Escape from Balder's Refuge (Guardian)"
-		},
-		3036: {
-			"es": "Sky Cruiser (Difícil)"
-		},
-		9770: {		
-			"es": "Ruinous Manor"
-		},
-		9970: {
-			"es": "Ruinous Manor (Difícil)"
-		},
-		3101: {
-			"es": "Gossamer Vault"
-		},
-		3104: {
-			"es": "Catalepticon"
-		},
-		3204: {
-			"es": "Catalepticon (Difícil)"
-		},
-		3037: {
-			"es": "Bahaar's Sanctum (Training)"
-		},
-		3040: {
-			"es": "Lumikan's Trial"
-		},
-		3106: {
-			"es": "Killing Grounds"
-		},
-		3206: {
-			"es": "Crimson Killing Grounds"
-		},
-		3032: {
-			"es": "Akalath Quarantine (Training)"
-		},
-		3034: {
-			"es": "Rampaging RK-9 Kennel"
-		},
-		3042: {
-			"es": "Gardan's Trial",
-		},
-		3041: {
-			"es": "Damned Citadel",
-		},
-		3030: {
-			"es": "Commander's Residence",
-		},
-		3043: {
-			"es": "Harnovog's Trial",
-		}
+	7011: {
+		"en": "Shadow of the Gutrends (Guardian)",
+		"ru": "Живодеры и черная тень (хранитель)",
+		"es": "Shadow of the Gutrends (Guardian)"
+	},
+	7015: {
+		"en": "Escape from Balder's Refuge (Guardian)",
+		"ru": "Спасти Убежище Балдера (хранитель)",
+		"es": "Escape from Balder's Refuge (Guardian)"
+	},
+	3032: {
+		"en": "Akalath Quarantine (Guide)",
+		"ru": "[Тренировка] Секретное подземелье крепости Берарк",
+		"es": "Akalath Quarantine (Training)"
+	},
+	3037: {
+		"en": "Bahaar's Sanctum (Guide)",
+		"ru": "[Тренировка] Святилище Бахаара",
+		"es": "Bahaar's Sanctum (Training)"
+	},
+	3040: {
+		"en": "Lumikan's Trial",
+		"ru": "Сонный паралич Лукмии (Подземелье испытаний)",
+		"es": "Lumikan's Trial"
+	},
+	3042: {
+		"en": "Gardan's Trial",
+		"ru": "Место казни (Подземелье испытаний)",
+		"es": "Gardan's Trial"
+	},
+	3204: {
+		"en": "Catalepticon (Hard)",
+		"es": "Catalepticon (Difícil)"
+	},
+	9970: {
+		"en": "Ruinous Manor (Hard)",
+		"es": "Ruinous Manor (Difícil)"
+	},
+	3036: {
+		"en": "Sky Cruiser (Hard)",
+		"es": "Sky Cruiser (Difícil)"
+	},
+	3202: {
+		"en": "Draakon Arena (Hard)",
+		"es": "Draakon Arena (Difícil)"
+	},
+	3126: {
+		"en": "Corrupted Skynest (Hard)",
+		"es": "Corrupted Skynest (Difícil)"
+	},
+	3201: {
+		"en": "Gossamer Vault (Hard)",
+		"es": "Gossamer Vault (Difícil)"
+	},
+	9982: {
+		"en": "Grotto of Lost Souls (Hard)",
+		"es": "Grotto of Lost Souls (Difícil)"
+	},
+	9920: {
+		"en": "Antaroth's Abyss (Hard)",
+		"es": "Antaroth's Abyss (Difícil)"
+	}
 };
