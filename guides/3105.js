1e9118a5d9f37e7e236964b1e9c1e47d3da0c62b
Loliconera <69399372+Loliconera@users.noreply.github.com>
2021-08-03 17:26:57 -0500

    Actualizar Fusion Laboratory y Hard

--- guides/3105.js
+++ guides/3105.js
@@ -4,15 +4,18 @@
 
 module.exports = (dispatch, handlers, guide, lang) => {
 
+	let print_lasers = true;
+
 	return {
+		"ns-3105-1000": [
+			{ type: "func", func: () => print_lasers = true }
+		],
 		"nd-3105-1000": [
 			{ type: "stop_timers" },
 			{ type: "despawn_all" }
 		],
 
 		"h-3105-1000-80": [{ type: "text", sub_type: "message", message: "80%", message_ES: "80%" }],
-		// "h-3105-1000-50": [{ type: "text", sub_type: "message", message: "50%", message_ES: "50%" }],
-		// "h-3105-1000-40": [{ type: "text", sub_type: "message", message: "40%", message_ES: "40%" }],
 		"h-3105-1000-35": [{ type: "text", sub_type: "message", message: "35%", message_ES: "35%" }],
 
 		"s-3105-1000-101-0": [{ type: "text", sub_type: "message", message: "Front Swing", message_ES: "Ataque Frontal" }],
@@ -29,15 +32,15 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		],
 		"s-3105-1000-108-0": [
 			{ type: "text", sub_type: "message", message: "Push (Tank)", message_ES: "Empujar (Tank)" },
-			{ type: "spawn", func: "vector", args: [553, 90, 60, 0, 500, 0, 3000] },
-			{ type: "spawn", func: "vector", args: [553, 270, 60, 0, 500, 0, 3000] }
+			{ type: "spawn", func: "vector", args: [553, 90, 80, 0, 300, 0, 3000] },
+			{ type: "spawn", func: "vector", args: [553, 270, 80, 0, 300, 0, 3000] }
 		],
 		"s-3105-1000-109-0": [
 			{ type: "text", sub_type: "message", message: "Front Laser", message_ES: "Láser Frontal" },
 			{ type: "spawn", func: "vector", args: [553, 90, 60, 0, 500, 1000, 4000] },
 			{ type: "spawn", func: "vector", args: [553, 270, 60, 0, 500, 1000, 4000] }
 		],
-		"s-3105-1000-106-0": [{ type: "text", sub_type: "message", message: "Blades Front", message_ES: "Cuchillas Frontal" }],
+		"s-3105-1000-106-0": [{ type: "text", sub_type: "message", message: "Blades Front", message_ES: "Cuchillas Frontales" }],
 		"s-3105-1000-112-0": [
 			{ type: "text", sub_type: "message", message: "Blades Back", message_ES: "Cuchillas hacia atras" },
 			{ type: "spawn", func: "vector", args: [553, 70, 10, 160, 350, 0, 3000] },
@@ -67,29 +70,30 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 18, 180, 1500, 5000] },
 			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 12, 360, 1500, 5000] },
 			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 10, 550, 1500, 5000] },
-			{ type: "text", sub_type: "message", delay: 5000, message: "Dodge", message_ES: "Iframe" }
+			{ type: "text", sub_type: "message", delay: 4900, message: "Dodge", message_ES: "Iframe" }
 		],
 		"s-3105-1000-116-0": [
 			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 18, 180, 1500, 5000] },
 			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 12, 360, 1500, 5000] },
 			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 10, 550, 1500, 5000] },
-			{ type: "text", sub_type: "message", delay: 5000, message: "Dodge", message_ES: "Iframe" }
+			{ type: "text", sub_type: "message", delay: 4900, message: "Dodge", message_ES: "Iframe" }
 		],
 
 		"qb-3105-1000-31051002": [{ type: "text", sub_type: "message", message: "Shield!", message_ES: "¡Escudo!" }], // <50%
 		"s-3105-1000-123-0": [{ type: "text", sub_type: "message", message: "Break Failure", message_ES: "Escudo No Perforado" }],
 
-		"qb-3105-1000-31051004": [{ type: "text", sub_type: "message", message: "Bait (Lasers)", message_ES: "Bait (Láseres)" }], // range check
-		"qb-3105-1000-31051005": [{ type: "text", sub_type: "message", message: "Bait (Storm)", message_ES: "Bait (Tormenta)" }], // get stun
-		"qb-3105-1000-31051006": [{ type: "text", sub_type: "message", message: "Bait (Blades)", message_ES: "Bait (Cuchillas)" }], // cast ress
+		"qb-3105-1000-31051004": [{ type: "text", sub_type: "message", message: "Bait", message_ES: "Bait" }], // range check
+		"qb-3105-1000-31051005": [{ type: "text", sub_type: "message", message: "Bait", message_ES: "Bait" }], // get stun
+		"qb-3105-1000-31051006": [{ type: "text", sub_type: "message", message: "Bait", message_ES: "Bait" }], // cast ress
 		"qb-3105-1000-31051007": [{ type: "text", sub_type: "message", message: "Puddles", message_ES: "Charcos" }], // <40%
 
 		// Core mech <35%
-		"qb-3105-1000-31051010": [{ type: "text", sub_type: "message", message: "Lasers + Wave", message_ES: "Láseres + Ola" }],
+		"qb-3105-1000-31051010": [
+			{ type: "text", sub_type: "message", message: "LASERS + WAVE", message_ES: "LASERES + OLA" },
+			{ type: "text", sub_type: "notification", message: "LASERS + WAVE", message_ES: "LASERES + OLA", speech: false }
+		],
 		"s-3105-1000-304-0": [ // red
 			{ type: "text", sub_type: "message", message: "Dodge!", message_ES: "Iframe!", delay: 200 },
-			// { type: "text", sub_type: "message", message: "Plague of Exhaustion", message_ES: "Чума/регресс", class_position: "priest", delay: 1000 },
-			// { type: "text", sub_type: "message", message: "Regression", message_ES: "Регресс", class_position: "mystic", delay: 1000 },
 			{ type: "text", sub_type: "message", message: "Give Stun!", message_ES: "¡Usar Stun!", delay: 2000 }
 		],
 		"s-3105-1000-305-0": [ // blue
@@ -99,23 +103,35 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-3105-1000-121-0": [{ type: "text", sub_type: "message", message: "AoE", message_ES: "АоЕ" }], // red
 		"s-3105-1000-122-0": [{ type: "text", sub_type: "message", message: "AoE", message_ES: "АоЕ" }], // blue
 
-		"qb-3105-1000-31051011": [{ type: "text", sub_type: "message", message: "Lasers!", message_ES: "Láseres!" }], // <80%
+		// Lasers <80%
+		"qb-3105-1001-31051011": [
+			{ type: "event", check_func: () => print_lasers, args: [
+				{ type: "text", sub_type: "message", message: "Lasers!", message_ES: "¡Láseres!" },
+				{ type: "func", func: () => print_lasers = false },
+				{ type: "func", func: () => print_lasers = true, delay: 16000 }
+			] }
+		],
+		"qb-3105-1002-31051011": "qb-3105-1001-31051011",
+		"qb-3105-1003-31051011": "qb-3105-1001-31051011",
+		"qb-3105-1004-31051011": "qb-3105-1001-31051011",
+		"qb-3105-1005-31051011": "qb-3105-1001-31051011",
+		"qb-3105-1006-31051011": "qb-3105-1001-31051011",
 
-		"s-3105-1001-101-0": [{ type: "spawn", func: "vector", args: [912, 0, 0, 0, 3000, 0, 5000] }], // laser (basic)
+		"s-3105-1001-101-0": [{ type: "spawn", func: "vector", args: [912, 0, 0, 0, 3000, 1000, 4000] }], // laser (basic)
 		"s-3105-1002-101-0": "s-3105-1001-101-0",
 		"s-3105-1003-101-0": "s-3105-1001-101-0",
 		"s-3105-1004-101-0": "s-3105-1001-101-0",
 		"s-3105-1005-101-0": "s-3105-1001-101-0",
 		"s-3105-1006-101-0": "s-3105-1001-101-0",
 
-		"s-3105-1001-102-0": [{ type: "spawn", func: "vector", args: [912, 0, 0, 0, 3000, 0, 5000] }], // laser (bait)
+		"s-3105-1001-102-0": [{ type: "spawn", func: "vector", args: [912, 0, 0, 0, 3000, 1000, 4000] }], // laser (bait)
 		"s-3105-1002-102-0": "s-3105-1001-102-0",
 		"s-3105-1003-102-0": "s-3105-1001-102-0",
 		"s-3105-1004-102-0": "s-3105-1001-102-0",
 		"s-3105-1005-102-0": "s-3105-1001-102-0",
 		"s-3105-1006-102-0": "s-3105-1001-102-0",
 
-		"s-3105-1001-103-0": [{ type: "spawn", func: "vector", args: [912, 0, 0, 0, 3000, 0, 5000] }], // laser (core)
+		"s-3105-1001-103-0": [{ type: "spawn", func: "vector", args: [912, 0, 0, 0, 3000, 1000, 4000] }], // laser (core)
 		"s-3105-1002-103-0": "s-3105-1001-103-0",
 		"s-3105-1003-103-0": "s-3105-1001-103-0",
 		"s-3105-1004-103-0": "s-3105-1001-103-0",
--- guides/3205.js
+++ guides/3205.js
@@ -4,7 +4,12 @@
 
 module.exports = (dispatch, handlers, guide, lang) => {
 
+	let print_lasers = true;
+
 	return {
+		"ns-3205-1000": [
+			{ type: "func", func: () => print_lasers = true }
+		],
 		"nd-3205-1000": [
 			{ type: "stop_timers" },
 			{ type: "despawn_all" }
@@ -29,8 +34,8 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		],
 		"s-3205-1000-108-0": [
 			{ type: "text", sub_type: "message", message: "Push (Tank)", message_ES: "Empujar (Tank)" },
-			{ type: "spawn", func: "vector", args: [553, 90, 60, 0, 500, 0, 3000] },
-			{ type: "spawn", func: "vector", args: [553, 270, 60, 0, 500, 0, 3000] }
+			{ type: "spawn", func: "vector", args: [553, 90, 80, 0, 300, 0, 3000] },
+			{ type: "spawn", func: "vector", args: [553, 270, 80, 0, 300, 0, 3000] }
 		],
 		"s-3205-1000-109-0": [
 			{ type: "text", sub_type: "message", message: "Front Laser", message_ES: "Láser Frontal" },
@@ -67,25 +72,28 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 18, 180, 1500, 5000] },
 			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 12, 360, 1500, 5000] },
 			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 10, 550, 1500, 5000] },
-			{ type: "text", sub_type: "message", delay: 5000, message: "Dodge", message_ES: "Iframe" }
+			{ type: "text", sub_type: "message", delay: 4900, message: "Dodge", message_ES: "Iframe" }
 		],
 		"s-3205-1000-116-0": [
 			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 18, 180, 1500, 5000] },
 			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 12, 360, 1500, 5000] },
 			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 10, 550, 1500, 5000] },
-			{ type: "text", sub_type: "message", delay: 5000, message: "Dodge", message_ES: "Iframe" }
+			{ type: "text", sub_type: "message", delay: 4900, message: "Dodge", message_ES: "Iframe" }
 		],
 
 		"qb-3205-1000-32051002": [{ type: "text", sub_type: "message", message: "Shield!", message_ES: "¡ESCUDO!" }], // <50%
 		"s-3205-1000-123-0": [{ type: "text", sub_type: "message", message: "Break Failure", message_ES: "ESCUDO No Perforado" }],
 
-		"qb-3205-1000-32051004": [{ type: "text", sub_type: "message", message: "Bait (Lasers)", message_ES: "Bait (Láseres)" }], // range check
-		"qb-3205-1000-32051005": [{ type: "text", sub_type: "message", message: "Bait (Storm)", message_ES: "Bait (Tormenta)" }], // get stun
-		"qb-3205-1000-32051006": [{ type: "text", sub_type: "message", message: "Bait (Blades)", message_ES: "Bait (Cuchillas)" }], // cast ress
+		"qb-3205-1000-32051004": [{ type: "text", sub_type: "message", message: "Bait", message_ES: "Bait" }], // range check
+		"qb-3205-1000-32051005": [{ type: "text", sub_type: "message", message: "Bait", message_ES: "Bait" }], // get stun
+		"qb-3205-1000-32051006": [{ type: "text", sub_type: "message", message: "Bait", message_ES: "Bait" }], // cast ress
 		"qb-3205-1000-32051007": [{ type: "text", sub_type: "message", message: "Puddles", message_ES: "Charcos" }], // <40%
 
 		// Core mech <35%
-		"qb-3205-1000-32051010": [{ type: "text", sub_type: "message", message: "Lasers + Wave", message_ES: "Láseres + Ola" }],
+		"qb-3205-1000-32051010": [
+			{ type: "text", sub_type: "message", message: "LASERS + WAVE", message_ES: "LASERES + OLA" },
+			{ type: "text", sub_type: "notification", message: "LASERS + WAVE", message_ES: "LASERES + OLA", speech: false }
+		],
 		"s-3205-1000-304-0": [ // red
 			{ type: "text", sub_type: "message", message: "Dodge!", message_ES: "Iframe!", delay: 200 },
 			{ type: "text", sub_type: "message", message: "Plague of Exhaustion", message_ES: "Plague", class_position: "priest", delay: 1000 },
@@ -99,23 +107,35 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-3205-1000-121-0": [{ type: "text", sub_type: "message", message: "AoE", message_ES: "АоЕ" }], // red
 		"s-3205-1000-122-0": [{ type: "text", sub_type: "message", message: "AoE", message_ES: "АоЕ" }], // blue
 
-		"qb-3205-1000-32051011": [{ type: "text", sub_type: "message", message: "Lasers!", message_ES: "Láseres!" }], // <80%
+		// Lasers <80%
+		"qb-3205-1001-32051011": [
+			{ type: "event", check_func: () => print_lasers, args: [
+				{ type: "text", sub_type: "message", message: "Lasers!", message_ES: "¡Láseres!" },
+				{ type: "func", func: () => print_lasers = false },
+				{ type: "func", func: () => print_lasers = true, delay: 16000 }
+			] }
+		],
+		"qb-3205-1002-32051011": "qb-3205-1001-32051011",
+		"qb-3205-1003-32051011": "qb-3205-1001-32051011",
+		"qb-3205-1004-32051011": "qb-3205-1001-32051011",
+		"qb-3205-1005-32051011": "qb-3205-1001-32051011",
+		"qb-3205-1006-32051011": "qb-3205-1001-32051011",
 
-		"s-3205-1001-101-0": [{ type: "spawn", func: "vector", args: [912, 0, 0, 0, 3000, 0, 5000] }], // laser (basic)
+		"s-3205-1001-101-0": [{ type: "spawn", func: "vector", args: [912, 0, 0, 0, 3000, 1000, 4000] }], // laser (basic)
 		"s-3205-1002-101-0": "s-3205-1001-101-0",
 		"s-3205-1003-101-0": "s-3205-1001-101-0",
 		"s-3205-1004-101-0": "s-3205-1001-101-0",
 		"s-3205-1005-101-0": "s-3205-1001-101-0",
 		"s-3205-1006-101-0": "s-3205-1001-101-0",
 
-		"s-3205-1001-102-0": [{ type: "spawn", func: "vector", args: [912, 0, 0, 0, 3000, 0, 5000] }], // laser (bait)
+		"s-3205-1001-102-0": [{ type: "spawn", func: "vector", args: [912, 0, 0, 0, 3000, 1000, 4000] }], // laser (bait)
 		"s-3205-1002-102-0": "s-3205-1001-102-0",
 		"s-3205-1003-102-0": "s-3205-1001-102-0",
 		"s-3205-1004-102-0": "s-3205-1001-102-0",
 		"s-3205-1005-102-0": "s-3205-1001-102-0",
 		"s-3205-1006-102-0": "s-3205-1001-102-0",
 
-		"s-3205-1001-103-0": [{ type: "spawn", func: "vector", args: [912, 0, 0, 0, 3000, 0, 5000] }], // laser (core)
+		"s-3205-1001-103-0": [{ type: "spawn", func: "vector", args: [912, 0, 0, 0, 3000, 1000, 4000] }], // laser (core)
 		"s-3205-1002-103-0": "s-3205-1001-103-0",
 		"s-3205-1003-103-0": "s-3205-1001-103-0",
 		"s-3205-1004-103-0": "s-3205-1001-103-0",
