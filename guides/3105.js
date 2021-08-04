cdabe2070f6919ebc5c9e8ea88b84a24fa9d98b5
Loliconera <69399372+Loliconera@users.noreply.github.com>
2021-08-04 18:28:05 -0500

    Add files via upload

--- guides/3105.js
+++ guides/3105.js
@@ -20,15 +20,15 @@ module.exports = (dispatch, handlers, guide, lang) => {
 
 		"s-3105-1000-101-0": [{ type: "text", sub_type: "message", message: "Front Swing", message_ES: "Ataque Frontal" }],
 		"s-3105-1000-102-0": [
-			{ type: "spawn", func: "vector", args: [553, 0, 0, 180, 300, 2000, 2000] },
+			{ type: "spawn", func: "vector", args: [553, 0, 0, 180, 200, 2000, 2000] },
 			{ type: "spawn", func: "vector", args: [553, 0, 0, 0, 300, 2000, 2000] },
 			{ type: "spawn", func: "vector", args: [553, 270, 150, 0, 300, 2000, 2000] },
-			{ type: "spawn", func: "vector", args: [553, 270, 150, 180, 300, 2000, 2000] },
+			{ type: "spawn", func: "vector", args: [553, 270, 150, 180, 200, 2000, 2000] },
 			{ type: "text", sub_type: "message", message: "Disc Throw", message_ES: "Lanzar disco" }
 		],
 		"s-3105-1000-104-0": [
 			{ type: "text", sub_type: "message", message: "Stun (AOE)", message_ES: "Stun (АОЕ)" },
-			{ type: "spawn", func: "circle", args: [true, 553, 0, 0, 12, 250, 100, 3000] }
+			{ type: "spawn", func: "circle", args: [true, 553, 0, 0, 12, 260, 100, 3000] }
 		],
 		"s-3105-1000-108-0": [
 			{ type: "text", sub_type: "message", message: "Push (Tank)", message_ES: "Empujar (Tank)" },
@@ -53,36 +53,37 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		],
 		"s-3105-1000-114-0": [
 			{ type: "text", sub_type: "message", message: "Back Laser", message_ES: "Láser hacia atras" },
-			{ type: "spawn", func: "vector", args: [553, 90, 60, 180, 500, 0, 2000] },
-			{ type: "spawn", func: "vector", args: [553, 270, 60, 180, 500, 0, 2000] }
+			{ type: "spawn", func: "vector", args: [553, 90, 60, 180, 500, 0, 3000] },
+			{ type: "spawn", func: "vector", args: [553, 270, 60, 180, 500, 0, 3000] }
 		],
 		"s-3105-1000-120-0": [{ type: "text", sub_type: "message", message: "Storm", message_ES: "Tormenta" }],
 		"s-3105-1000-107-0": [
 			{ type: "text", sub_type: "message", message: "Spin (Kaia) | Back Laser", message_ES: "Girar (Kaia) | Láser hacia atras" },
 			{ type: "spawn", func: "circle", args: [true, 553, 0, 0, 12, 260, 100, 5000] }
 		],
-		"s-3105-1000-303-0": [{ type: "text", sub_type: "message", message: "Kick (Push)", message_ES: "Patada (Empujar)" }],
+		"s-3105-1000-117-0": [{ type: "text", sub_type: "message", message: "Kick", message_ES: "Patada" }],
+		"s-3105-1000-118-0": [{ type: "text", sub_type: "message", message: "Kick (Dodge)", message_ES: "Patada (Iframe)" }],
 
 		// Donuts combo <80%
-		"qb-3105-1000-31051013": [{ type: "text", sub_type: "message", message: "Donuts: Out > In > Dodge", message_ES: "Donas: Salir > Entrar > Iframe" }],
-		"qb-3105-1000-31051014": [{ type: "text", sub_type: "message", message: "Donuts: In > Out > Dodge", message_ES: "Donas: Entrar > Salir > Iframe" }],
+		"qb-3105-1000-31051013": [{ type: "text", sub_type: "message", message: "Outward Donuts (Out > In)", message_ES: "Donas hacia afuera (Salir > Entrar)" }],
+		"qb-3105-1000-31051014": [{ type: "text", sub_type: "message", message: "Inward Donuts (In > Out)", message_ES: "Donas hacia adentro (Entrar > Salir)" }],
 		"s-3105-1000-115-0": [
 			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 18, 180, 1500, 5000] },
-			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 12, 360, 1500, 5000] },
+			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 12, 330, 1500, 5000] },
 			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 10, 550, 1500, 5000] },
-			{ type: "text", sub_type: "message", delay: 4900, message: "Dodge", message_ES: "Iframe" }
+			{ type: "text", sub_type: "message", delay: 4800, message: "Dodge", message_ES: "Iframe" }
 		],
 		"s-3105-1000-116-0": [
 			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 18, 180, 1500, 5000] },
-			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 12, 360, 1500, 5000] },
-			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 10, 550, 1500, 5000] },
-			{ type: "text", sub_type: "message", delay: 4900, message: "Dodge", message_ES: "Iframe" }
+			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 12, 330, 1500, 5000] },
+			{ type: "spawn", func: "circle", args: [false, 445, 0, 0, 10, 510, 1500, 5000] },
+			{ type: "text", sub_type: "message", delay: 4800, message: "Dodge", message_ES: "Iframe" }
 		],
 
 		"qb-3105-1000-31051002": [{ type: "text", sub_type: "message", message: "Shield!", message_ES: "¡Escudo!" }], // <50%
 		"s-3105-1000-123-0": [{ type: "text", sub_type: "message", message: "Break Failure", message_ES: "Escudo No Perforado" }],
 
-		"qb-3105-1000-31051004": [{ type: "text", sub_type: "message", message: "Bait", message_ES: "Bait" }], // range check
+		"qb-3105-1000-31051004": [{ type: "text", sub_type: "message", message: "Bait (Lasers)", message_ES: "Bait (Láseres)" }], // range check
 		"qb-3105-1000-31051005": [{ type: "text", sub_type: "message", message: "Bait", message_ES: "Bait" }], // get stun
 		"qb-3105-1000-31051006": [{ type: "text", sub_type: "message", message: "Bait", message_ES: "Bait" }], // cast ress
 		"qb-3105-1000-31051007": [{ type: "text", sub_type: "message", message: "Puddles", message_ES: "Charcos" }], // <40%
@@ -92,12 +93,14 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "text", sub_type: "message", message: "LASERS + WAVE", message_ES: "LASERES + OLA" },
 			{ type: "text", sub_type: "notification", message: "LASERS + WAVE", message_ES: "LASERES + OLA", speech: false }
 		],
+
+		"s-3105-1000-310-0": [{ type: "spawn", func: "marker", args: [false, 0, 0, 0, 8000, true, ["Lasers", "Wave"]] }],
 		"s-3105-1000-304-0": [ // red
-			{ type: "text", sub_type: "message", message: "Dodge!", message_ES: "Iframe!", delay: 200 },
+			{ type: "text", sub_type: "message", message: "Wave (dodge)", message_ES: "Olas (iframe)", delay: 100  },
 			{ type: "text", sub_type: "message", message: "Give Stun!", message_ES: "¡Usar Stun!", delay: 2000 }
 		],
 		"s-3105-1000-305-0": [ // blue
-			{ type: "text", sub_type: "message", message: "Dodge!", message_ES: "Iframe!", delay: 200 },
+			{ type: "text", sub_type: "message", message: "Wave (dodge)", message_ES: "Olas (iframe)", delay: 100  },
 			{ type: "text", sub_type: "message", message: "Give Stun!", message_ES: "¡Usar Stun!", delay: 2000 }
 		],
 		"s-3105-1000-121-0": [{ type: "text", sub_type: "message", message: "AoE", message_ES: "АоЕ" }], // red
@@ -131,7 +134,7 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-3105-1005-102-0": "s-3105-1001-102-0",
 		"s-3105-1006-102-0": "s-3105-1001-102-0",
 
-		"s-3105-1001-103-0": [{ type: "spawn", func: "vector", args: [912, 0, 0, 0, 3000, 1000, 4000] }], // laser (core)
+		"s-3105-1001-103-0": [{ type: "spawn", func: "vector", args: [912, 0, 0, 0, 3000, 0, 5000] }], // laser (core)
 		"s-3105-1002-103-0": "s-3105-1001-103-0",
 		"s-3105-1003-103-0": "s-3105-1001-103-0",
 		"s-3105-1004-103-0": "s-3105-1001-103-0",
--- lang/dungeons.js
+++ lang/dungeons.js
@@ -4,32 +4,26 @@
 module.exports = {
 	7011: {
 		"en": "Shadow of the Gutrends (Guardian)",
-		"ru": "Живодеры и черная тень (хранитель)",
 		"es": "Shadow of the Gutrends (Guardian)"
 	},
 	7015: {
 		"en": "Escape from Balder's Refuge (Guardian)",
-		"ru": "Спасти Убежище Балдера (хранитель)",
 		"es": "Escape from Balder's Refuge (Guardian)"
 	},
 	3032: {
 		"en": "Akalath Quarantine (Guide)",
-		"ru": "[Тренировка] Секретное подземелье крепости Берарк",
 		"es": "Akalath Quarantine (Training)"
 	},
 	3037: {
 		"en": "Bahaar's Sanctum (Guide)",
-		"ru": "[Тренировка] Святилище Бахаара",
 		"es": "Bahaar's Sanctum (Training)"
 	},
 	3040: {
 		"en": "Lumikan's Trial",
-		"ru": "Сонный паралич Лукмии (Подземелье испытаний)",
 		"es": "Lumikan's Trial"
 	},
 	3042: {
 		"en": "Gardan's Trial",
-		"ru": "Место казни (Подземелье испытаний)",
 		"es": "Gardan's Trial"
 	},
 	3204: {
