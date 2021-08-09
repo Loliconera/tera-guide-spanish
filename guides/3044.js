b29b0c94ed5f224f0c5624809a374c3bb042c631
Loliconera <69399372+Loliconera@users.noreply.github.com>
2021-08-09 13:31:28 -0500

    Add files via upload

--- guides/3044.js
+++ guides/3044.js
@@ -21,14 +21,14 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "spawn", func: "circle", args: [true, 553, 150, 380, null, 250, 0, 2000] }
 		],
 		"s-3044-1000-122-0": [ // 122 - 123 - 124
-			{ type: "text", sub_type: "message", message: "Throw (Objetivo)", message_ES: "Lanzar (Objetivo)" },
+			{ type: "text", sub_type: "message", message: "Throw (Target)", message_ES: "Lanzar (Objetivo)" },
 			{ type: "spawn", func: "circle", args: [true, 553, 0, 550, null, 180, 0, 2000] }
 		],
 		"s-3044-1000-123-0": [
 			{ type: "spawn", func: "vector", args: [553, 90, 80, 0, 500, 0, 2000] },
 			{ type: "spawn", func: "vector", args: [553, 270, 80, 0, 500, 0, 2000] }
 		],
-		"s-3044-1000-124-0": [{ type: "text", sub_type: "message", message: "AoE", message_ES: "АоЕ" }],
+		"s-3044-1000-124-0": [{ type: "text", sub_type: "message", message: "AoE", message_ES: "AoE" }],
 		"s-3044-1000-125-0": [{ type: "text", sub_type: "message", message: "Hit | Frontal Stun", message_ES: "Lanzar | Stun Frontal" }],
 		"s-3044-1000-126-0": [
 			{ type: "text", sub_type: "message", message: "Frontal Stun", message_ES: "Stun Frontal" },
@@ -41,7 +41,7 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "spawn", func: "circle", args: [true, 553, 180, 340, null, 250, 0, 2000] }
 		],
 		"s-3044-1000-142-0": [{ type: "text", sub_type: "message", message: "Frontal Combo", message_ES: "Frontal Combo" }],
-		"s-3044-1000-301-0": [{ type: "text", sub_type: "message", message: "Circles (Objetivo)", message_ES: "Círculos (Objetivo)" }],
+		"s-3044-1000-301-0": [{ type: "text", sub_type: "message", message: "Circles (Target)", message_ES: "Círculos (Objetivo)" }],
 		"s-3044-1000-340-0": [
 			{ type: "text", sub_type: "message", message: "Laser", message_ES: "Láser (Iframe)" },
 			{ type: "spawn", func: "vector", args: [912, 90, 50, 25, 300, 0, 2000] },
@@ -65,14 +65,14 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "spawn", func: "circle", args: [true, 553, 150, 380, null, 250, 0, 2000] }
 		],
 		"s-3044-2000-122-0": [ // 122 - 123 - 124
-			{ type: "text", sub_type: "message", message: "Throw (Objetivo)", message_ES: "Lanzar (Objetivo)" },
+			{ type: "text", sub_type: "message", message: "Throw (Target)", message_ES: "Lanzar (Objetivo)" },
 			{ type: "spawn", func: "circle", args: [true, 553, 0, 550, null, 180, 0, 2000] }
 		],
 		"s-3044-2000-123-0": [
 			{ type: "spawn", func: "vector", args: [553, 90, 80, 0, 500, 0, 2000] },
 			{ type: "spawn", func: "vector", args: [553, 270, 80, 0, 500, 0, 2000] }
 		],
-		"s-3044-2000-124-0": [{ type: "text", sub_type: "message", message: "AoE", message_ES: "АоЕ" }],
+		"s-3044-2000-124-0": [{ type: "text", sub_type: "message", message: "AoE", message_ES: "AoE" }],
 		"s-3044-2000-125-0": [{ type: "text", sub_type: "message", message: "Hit | Frontal Stun", message_ES: "Lanzar | Stun Frontal" }],
 		"s-3044-2000-126-0": [
 			{ type: "text", sub_type: "message", message: "Frontal Stun", message_ES: "Stun Frontal" },
@@ -120,19 +120,19 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "spawn", func: "vector", args: [553, 5, 500, 180, 1000, 2000, 2000] }
 		],
 		"s-3044-2000-148-0": [
-			{ type: "text", sub_type: "message", message: "Stun (AoE)", message_ES: "Stun (АоЕ)" },
+			{ type: "text", sub_type: "message", message: "Stun (AoE)", message_ES: "Stun (AoE)" },
 			{ type: "spawn", func: "circle", args: [true, 553, 180, 20, null, 250, 0, 2000] }
 		],
 		"s-3044-2000-151-0": [
-			{ type: "text", sub_type: "message", message: "Throws Combo | Stun (AoE)", message_ES: "Lanzar Combo | Stun (АоЕ)" },
+			{ type: "text", sub_type: "message", message: "Throws Combo | Stun (AoE)", message_ES: "Lanzar Combo | Stun (AoE)" },
 			{ type: "spawn", func: "circle", args: [true, 912, 180, 200, null, 200, 0, 3000] }
 		],
 		"s-3044-2000-152-0": [
-			{ type: "text", sub_type: "message", message: "Stun (AoE)", message_ES: "Stun (АоЕ)" },
+			{ type: "text", sub_type: "message", message: "Stun (AoE)", message_ES: "Stun (AoE)" },
 			{ type: "spawn", func: "circle", args: [true, 553, 180, 20, null, 250, 0, 2000] }
 		],
-		"s-3044-2000-301-0": [{ type: "text", sub_type: "message", message: "Circles (Objetivo)", message_ES: "Círculos (Objetivo)" }], // normal
-		"s-3044-2000-302-0": [{ type: "text", sub_type: "message", message: "Circles (Objetivo)", message_ES: "Círculos (Objetivo)" }], // fast
+		"s-3044-2000-301-0": [{ type: "text", sub_type: "message", message: "Circles (Target)", message_ES: "Círculos (Objetivo)" }], // normal
+		"s-3044-2000-302-0": [{ type: "text", sub_type: "message", message: "Circles (Target)", message_ES: "Círculos (Objetivo)" }], // fast
 		"s-3044-2000-303-0": [
 			{ type: "text", sub_type: "message", message: "Semicircle (Left Safe)", message_ES: "Semicírculo (Izquierda Segura)" },
 			{ type: "spawn", func: "marker", args: [false, 250, 300, 0, 3000, true, null] },
