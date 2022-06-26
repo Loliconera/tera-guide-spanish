7245d49fb4c0f92dee923585157b6b8cd121c58a
Loliconera <69399372+Loliconera@users.noreply.github.com>
2022-06-25 20:36:47 -0500

    Arreglar formatos de textos

--- lang/strings.js
+++ lang/strings.js
@@ -7,7 +7,7 @@ module.exports.general = {
 	// Español
 	es: {
 		unknowncommand: "Comando desconocido, Para ver todos los comandos escriba <font color='#ffff00'>!guia help</font>",
-		helpheader: "Use el comando <font color='#ffff00'>!guia gui </font> para configurar tu Guía <font color='#00a6ff'>\nGuía en idioma </font><font color='#00bb2d'>ES</font><font color='#FFFFFF'>PA</font><font color='#FF0000'>ÑOL</font><font color='#00a6ff'>, También disponible en idioma Inglés \nUse el comando </font><font color='#ffff00'>!guia en </font><font color='#00a6ff'>para cambiar al idioma</font> ‍‍‍‍<font color='#00ffff'>Inglés</font>",
+		helpheader: "Use el comando <font color='#ffff00'>!guia gui </font> para configurar tu Guía <font color='#00a6ff'>\nGuía en idioma </font><font color='#00bb2d'>ES</font><font color='#FFFFFF'>PA</font><font color='#FF0000'>ÑOL</font><font color='#00a6ff'>, También disponible en idioma Inglés \nUse el comando </font><font color='#ffff00'>!guia en </font><font color='#00a6ff'>para cambiar al idioma</font> <font color='#00ffff'>Inglés</font>",
 		helpbody: [
 			["guia, modulo on/off", "PRMSG"],
 			["guia gui, mostrar interfaz GUI", "PRMSG"],
