dcc553b29ceb3932083432353f76529c07f040fc
Loliconera <69399372+Loliconera@users.noreply.github.com>
2022-08-02 22:57:26 -0500

    Actualizar Guías Ruinous Manor Hard/Normal

--- guides/9770.js
+++ guides/9770.js
@@ -72,14 +72,12 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-770-3000-2102-0": [{ type: "text", sub_type: "message", message: "Left Hand", message_ES: "Mano Izquierda" }],
 		"s-770-3000-1101-0": [{ type: "text", sub_type: "message", message: "Right Hand", message_ES: "Mano Derecha" }],
 		"s-770-3000-2101-0": [{ type: "text", sub_type: "message", message: "Right Hand", message_ES: "Mano Derecha" }],
-		"s-770-3000-1103-0": [
-			{ type: "text", sub_type: "message", message: "Tail Slam", message_ES: "Coletazo Atrás" },
+		"s-770-3000-1103-0": [{ type: "text", sub_type: "message", message: "Tail Slam", message_ES: "Coletazo Atrás" },
 			{ type: "spawn", func: "vector", args: [553, 283, 127, 180, 500, 0, 1400] },
 			{ type: "spawn", func: "vector", args: [553, 77, 127, 180, 500, 0, 1400] },
 			{ type: "spawn", func: "vector", args: [553, 165, 488, 270, 200, 0, 1400] }
 		],
-		"s-770-3000-2103-0": [
-			{ type: "text", sub_type: "message", message: "Tail Slam", message_ES: "Coletazo Atrás" },
+		"s-770-3000-2103-0": [{ type: "text", sub_type: "message", message: "Tail Slam", message_ES: "Coletazo Atrás" },
 			{ type: "spawn", func: "vector", args: [553, 283, 127, 180, 500, 0, 1400] },
 			{ type: "spawn", func: "vector", args: [553, 77, 127, 180, 500, 0, 1400] },
 			{ type: "spawn", func: "vector", args: [553, 165, 488, 270, 200, 0, 1400] }
@@ -88,14 +86,12 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-770-3000-2301-0": [{ type: "text", sub_type: "message", message: "Circles", message_ES: "Círculos" }],
 		"s-770-3000-1106-0": [{ type: "text", sub_type: "message", message: "Front Swipe", message_ES: "Empuje Frontal (Iframe)" }],
 		"s-770-3000-2106-0": [{ type: "text", sub_type: "message", message: "Front Swipe", message_ES: "Empuje Frontal (Iframe)" }],
-		"s-770-3000-1110-0": [
-			{ type: "text", sub_type: "message", message: "Tail AOE (jump in front)", message_ES: "Cola AoE (saltar al frente)" },
+		"s-770-3000-1110-0": [{ type: "text", sub_type: "message", message: "Tail AOE", message_ES: "Cola AoE" },
 			{ type: "spawn", func: "semicircle", args: [26, 340, 553, 0, 0, 7, 630, 0, 3000] },
 			{ type: "spawn", func: "vector", args: [553, 4, 40, 338, 600, 0, 3000] },
 			{ type: "spawn", func: "vector", args: [553, -4, 40, -338, 600, 0, 3000] }
 		],
-		"s-770-3000-2110-0": [
-			{ type: "text", sub_type: "message", message: "Tail AOE (jump in front)", message_ES: "Cola AoE (saltar al frente)" },
+		"s-770-3000-2110-0": [{ type: "text", sub_type: "message", message: "Tail AOE", message_ES: "Cola AoE" },
 			{ type: "spawn", func: "semicircle", args: [26, 340, 553, 0, 0, 7, 630, 0, 3000] },
 			{ type: "spawn", func: "vector", args: [553, 4, 40, 338, 600, 0, 3000] },
 			{ type: "spawn", func: "vector", args: [553, -4, 40, -338, 600, 0, 3000] }
@@ -119,21 +115,12 @@ module.exports = (dispatch, handlers, guide, lang) => {
 		"s-770-3000-2116-0": "s-770-3000-1116-0",
 		"s-770-3000-2117-0": "s-770-3000-1117-0",
 		
-		"s-770-3000-1108-0": [
-			{ type: "text", sub_type: "message", message: "Tail", message_ES: "Coletazo Frontal" },
+		"s-770-3000-1108-0": [{ type: "text", sub_type: "message", message: "Tail", message_ES: "Coletazo Frontal" },
 			{ type: "spawn", func: "circle", args: [false, 553, -7, 280, 20, 155, 0, 2000] }
 		],
-		"s-770-3000-2108-0": [
-			{ type: "text", sub_type: "message", message: "Tail", message_ES: "Coletazo Frontal" },
+		"s-770-3000-2108-0": [{ type: "text", sub_type: "message", message: "Tail", message_ES: "Coletazo Frontal" },
 			{ type: "spawn", func: "circle", args: [false, 553, -7, 280, 20, 155, 0, 2000] }
 		],
-		"s-770-3000-1322-0": [
-			{ type: "text", sub_type: "message", message: "Dodge!", message_ES: "¡Iframe!" },
-			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 14, 230, 0, 5000] },
-			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 12, 430, 0, 5000] },
-			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 8, 630, 0, 5000] }
-		],
-		"s-770-3000-1311-0": [{ type: "text", sub_type: "message", message: "Gather for Cleanse!", message_ES: "¡Cleanse!" }],
 		"s-770-3000-1120-0": [{ type: "text", sub_type: "message", message: "Shooting Skulls (Triple)", message_ES: "Laser (Triple)" }],
 		"s-770-3000-2120-0": [{ type: "text", sub_type: "message", message: "Shooting Skulls (Triple)", message_ES: "Laser (Triple)" }],
 		"s-770-3000-1121-0": [{ type: "text", sub_type: "message", message: "Shooting Skulls (Doble)", message_ES: "Laser (Doble)" }],
